75cbaad39f66ad99d94d1967913ffa3be314d344
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 20:23:33 +0000

    typo.

--- src/net/hillsdon/svnwiki/wiki/renderer/macro/ResultFormat.java
+++ src/net/hillsdon/svnwiki/wiki/renderer/macro/ResultFormat.java
@@ -3,7 +3,7 @@ package net.hillsdon.svnwiki.wiki.renderer.macro;
 /**
  * Result of a macro may be in any of these formats.
  * 
- * Non-XHTML will be rendered to XHTMl.
+ * Non-XHTML will be rendered to XHTML.
  * 
  * @author mth
  */
