e9dab66bcffcf685aed2aa543428ebc97fac949c
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 19:25:22 +0000

    Fixed and enhanced test to cover tagging of wikiwords with classes that indicate if they're existing pages.

--- src/net/hillsdon/svnwiki/wiki/TestRadeoxMarkupRenderer.java
+++ src/net/hillsdon/svnwiki/wiki/TestRadeoxMarkupRenderer.java
@@ -1,6 +1,5 @@
 package net.hillsdon.svnwiki.wiki;
 
-import java.io.IOException;
 import java.io.StringWriter;
 
 import junit.framework.TestCase;
@@ -8,12 +7,13 @@ import net.hillsdon.svnwiki.vc.SimplePageStore;
 
 public class TestRadeoxMarkupRenderer extends TestCase {
 
-  public void testIdentifiesWikiWords() throws IOException {
+  public void testIdentifiesWikiWords() throws Exception {
     SimplePageStore pageStore = new SimplePageStore();
+    pageStore.set("ExistingPage", -1, "content");
     MarkupRenderer renderer = new RadeoxMarkupRenderer(pageStore);
     StringWriter writer = new StringWriter();
-    renderer.render("A WikiWord is in this sentence.", writer);
-    assertEquals("A <a href='WikiWord'>WikiWord</a> is in this sentence.", writer.toString());
+    renderer.render("An ExistingPage and a PageWeStillNeedToWrite.", writer);
+    assertEquals("An <a class='existing-page' href='ExistingPage'>ExistingPage</a> and a <a class='new-page' href='PageWeStillNeedToWrite'>PageWeStillNeedToWrite</a>.", writer.toString());
   }
   
 }
