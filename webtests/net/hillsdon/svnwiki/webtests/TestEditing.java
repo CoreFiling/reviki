5f23e409de6c20512395ea07e6c8b2982919ed8c
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 19:37:43 +0000

    Test revision shown in title increments after an edit.

---
+++ webtests/net/hillsdon/svnwiki/webtests/TestEditing.java
@@ -0,0 +1,27 @@
+package net.hillsdon.svnwiki.webtests;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class TestEditing extends WebTestSupport {
+  
+  private static final Pattern RE_REVISION = Pattern.compile("r[0-9]+");
+  
+  public void testEditPageIncrementsRevision() throws Exception {
+    String name = uniqueWikiPageName("EditPageTest");
+    HtmlPage initial = editWikiPage(name, "Initial content", true);
+    long initialRevision = getRevisionNumberFromTitle(initial);
+    HtmlPage edited = editWikiPage(name, "Initial content.  Extra content.", false);
+    assertEquals(initialRevision + 1, getRevisionNumberFromTitle(edited));
+  }
+
+  private long getRevisionNumberFromTitle(HtmlPage initial) {
+    Matcher matcher = RE_REVISION.matcher(initial.getTitleText());
+    assertTrue(matcher.find());
+    long revision = Long.parseLong(matcher.group().substring(1));
+    return revision;
+  }
+  
+}
