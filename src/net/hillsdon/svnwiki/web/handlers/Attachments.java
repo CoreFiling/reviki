e96f40f763bd1918a008ff4e054c7e3f8292ec2a
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 19:56:42 +0000

    Web dispatch is now a chain where a bit of path is eaten at each step.  This made various bits nicer and ought to make sub-wiki stuff easy.

---
+++ src/net/hillsdon/svnwiki/web/handlers/Attachments.java
@@ -0,0 +1,38 @@
+package net.hillsdon.svnwiki.web.handlers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.hillsdon.svnwiki.vc.PageReference;
+import net.hillsdon.svnwiki.vc.PageStore;
+import net.hillsdon.svnwiki.web.ConsumedPath;
+
+public class Attachments extends PageRequestHandler {
+
+  private final PageRequestHandler _list;
+  private final PageRequestHandler _upload;
+  private final PageRequestHandler _get;
+
+  public Attachments(final PageStore pageStore) {
+    super(pageStore);
+    _list = new ListAttachments(pageStore);
+    _upload = new UploadAttachment(pageStore);
+    _get = new GetAttachment(pageStore);
+  }
+
+  @Override
+  public void handlePage(final ConsumedPath path, final HttpServletRequest request, final HttpServletResponse response, final PageReference page) throws Exception {
+    if (path.hasNext()) {
+      _get.handlePage(path, request, response, page);
+    }
+    else {
+      if (request.getMethod().equals("POST")) {
+        _upload.handlePage(path, request, response, page);
+      }
+      else {
+        _list.handlePage(path, request, response, page);
+      }
+    }
+  }
+
+}
