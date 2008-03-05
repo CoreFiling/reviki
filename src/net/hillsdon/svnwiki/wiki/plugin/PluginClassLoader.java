f8fe23da7b6e739af39e83b2e25a95816c2d5c5f
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 20:43:30 +0000

    The plugin classloader adds nested jars to the classpath too.

--- src/net/hillsdon/svnwiki/wiki/plugin/PluginClassLoader.java
+++ src/net/hillsdon/svnwiki/wiki/plugin/PluginClassLoader.java
@@ -1,13 +1,84 @@
 package net.hillsdon.svnwiki.wiki.plugin;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.Enumeration;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
+import org.apache.commons.io.IOUtils;
+
+/**
+ * A really simple system for plugins.
+ * 
+ * The relative entries in manifest entry Class-Path are assumed to point to jars
+ * in the root of the plugin jar.
+ * 
+ * The classes constributed by this plugin are defined by the Plugin-Contributions
+ * manifest parameter (space separated).
+ * 
+ * @author mth
+ */
 public class PluginClassLoader extends URLClassLoader {
 
-  public PluginClassLoader(final URL jarUrl, final ClassLoader parent) {
+  public PluginClassLoader(final URL jarUrl, final ClassLoader parent) throws InvalidPluginException {
     super(new URL[] {jarUrl}, parent);
-    findResource("/META-INF/MANIFEST.MF");
+    try {
+      URL last = getManifestURL(jarUrl);
+      
+      Manifest manifest = new Manifest(last.openStream());
+      Attributes attrs = manifest.getMainAttributes();
+      
+      String[] classPathEntries = attrs.getValue("Class-Path").split("\\s");
+      cacheClassPathEntries(jarUrl, classPathEntries);
+      String[] pluginContributions = attrs.getValue("Plugin-Contributions").split("\\s");
+      loadPluginContributionClasses(pluginContributions);
+    }
+    catch (URISyntaxException ex) {
+      throw new InvalidPluginException(ex);
+    }
+    catch (IOException ex) {
+      throw new InvalidPluginException(ex);
+    }
+  }
+
+  private void loadPluginContributionClasses(final String[] pluginContributions) {
+  }
+
+  private void cacheClassPathEntries(final URL jarUrl, final String[] classPathEntries) throws IOException {
+    for (String entry : classPathEntries) {
+      URL entryUrl = new URL("jar:" + jarUrl.toString() + "!/" + entry);
+      File file = File.createTempFile("cached-", entry);
+      file.deleteOnExit();
+      InputStream in = entryUrl.openStream();
+      FileOutputStream out = null;
+      try {
+        out = new FileOutputStream(file);
+        IOUtils.copy(in, out);
+      }
+      catch (IOException ex) {
+        IOUtils.closeQuietly(in);
+        IOUtils.closeQuietly(out);
+      }
+      addURL(file.toURI().toURL());
+    }
+  }
+
+  private URL getManifestURL(final URL jarUrl) throws IOException, URISyntaxException, InvalidPluginException {
+    Enumeration<URL> enumeration = getResources("META-INF/MANIFEST.MF");
+    URL last = null;
+    while (enumeration.hasMoreElements()) {
+      last = enumeration.nextElement();
+    }
+    if (last == null) {
+      throw new InvalidPluginException("No manifest found in given jar.");
+    }
+    return last;
   }
 
 }
--- src/net/hillsdon/svnwiki/wiki/plugin/TestPluginClassLoader.java
+++ src/net/hillsdon/svnwiki/wiki/plugin/TestPluginClassLoader.java
@@ -5,12 +5,13 @@ import junit.framework.TestCase;
 public class TestPluginClassLoader extends TestCase {
 
   private static final String EXAMPLE_PLUGIN_JAR = "test-plugin.jar";
-  private static final String EXAMPLE_CLASS_NAME = "net.hillsdon.svnwiki.test.plugin.Example";
+  private static final String DIRECTLY_IN_JAR_CLASS_NAME = "net.hillsdon.svnwiki.test.plugin.Example";
+  private static final String IN_EMBEDDED_JAR_CLASS_NAME = "net.hillsdon.svnwiki.test.plugin.Dependency";
 
   public void test() throws Exception {
     PluginClassLoader classloader = new PluginClassLoader(getClass().getResource(EXAMPLE_PLUGIN_JAR), getClass().getClassLoader());
-    Class<?> clazz = classloader.loadClass(EXAMPLE_CLASS_NAME);
-    assertSame(classloader, clazz.getClassLoader());
+    assertSame(classloader, classloader.loadClass(DIRECTLY_IN_JAR_CLASS_NAME).getClassLoader());
+    assertSame(classloader, classloader.loadClass(IN_EMBEDDED_JAR_CLASS_NAME).getClassLoader());
   }
   
 }
