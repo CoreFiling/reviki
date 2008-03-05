3e9e84c7d5c956c1ce2d8684c1f1a59472837c3e
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 20:43:08 +0000

    Extract some constants.

--- src/net/hillsdon/svnwiki/wiki/plugin/TestPluginClassLoader.java
+++ src/net/hillsdon/svnwiki/wiki/plugin/TestPluginClassLoader.java
@@ -4,9 +4,12 @@ import junit.framework.TestCase;
 
 public class TestPluginClassLoader extends TestCase {
 
+  private static final String EXAMPLE_PLUGIN_JAR = "test-plugin.jar";
+  private static final String EXAMPLE_CLASS_NAME = "net.hillsdon.svnwiki.test.plugin.Example";
+
   public void test() throws Exception {
-    PluginClassLoader classloader = new PluginClassLoader(getClass().getResource("test-plugin.jar"), getClass().getClassLoader());
-    Class<?> clazz = classloader.loadClass("net.hillsdon.svnwiki.test.plugin.Example");
+    PluginClassLoader classloader = new PluginClassLoader(getClass().getResource(EXAMPLE_PLUGIN_JAR), getClass().getClassLoader());
+    Class<?> clazz = classloader.loadClass(EXAMPLE_CLASS_NAME);
     assertSame(classloader, clazz.getClassLoader());
   }
   
