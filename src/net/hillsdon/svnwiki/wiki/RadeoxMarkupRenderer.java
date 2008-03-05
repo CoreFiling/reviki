9695c16d2beb58f673488859c156d5c34f9822c7
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 19:24:06 +0000

    Setting the RenderingEngine on the RenderingContext makes the wiki links work.  Fixes test.

--- src/net/hillsdon/svnwiki/wiki/RadeoxMarkupRenderer.java
+++ src/net/hillsdon/svnwiki/wiki/RadeoxMarkupRenderer.java
@@ -2,12 +2,10 @@ package net.hillsdon.svnwiki.wiki;
 
 import java.io.IOException;
 import java.io.Writer;
-import java.util.Locale;
 
 import net.hillsdon.svnwiki.vc.PageStore;
 
 import org.radeox.api.engine.RenderEngine;
-import org.radeox.api.engine.context.InitialRenderContext;
 import org.radeox.api.engine.context.RenderContext;
 import org.radeox.engine.context.BaseRenderContext;
 import org.radeox.filter.WikiLinkFilter;
@@ -18,13 +16,13 @@ public class RadeoxMarkupRenderer implements MarkupRenderer {
 
   public RadeoxMarkupRenderer(final PageStore store) {
     _engine = new SvnWikiRenderEngine(store);
-    _engine.getInitialRenderContext().set(InitialRenderContext.INPUT_LOCALE, Locale.US);
-    _engine.getInitialRenderContext().set(InitialRenderContext.OUTPUT_LOCALE, Locale.US);
+    _engine.getInitialRenderContext().setRenderEngine(_engine);
     _engine.getInitialRenderContext().getFilterPipe().addFilter(new WikiLinkFilter());
   }
   
   public void render(final String in, final Writer out) throws IOException {
     RenderContext context = new BaseRenderContext();
+    context.setRenderEngine(_engine);
     _engine.render(out, in, context);
   }
   
--- src/net/hillsdon/svnwiki/wiki/TestRadeoxMarkupRenderer.java
+++ src/net/hillsdon/svnwiki/wiki/TestRadeoxMarkupRenderer.java
@@ -8,12 +8,12 @@ import net.hillsdon.svnwiki.vc.SimplePageStore;
 
 public class TestRadeoxMarkupRenderer extends TestCase {
 
-  public void test() throws IOException {
+  public void testIdentifiesWikiWords() throws IOException {
     SimplePageStore pageStore = new SimplePageStore();
     MarkupRenderer renderer = new RadeoxMarkupRenderer(pageStore);
     StringWriter writer = new StringWriter();
     renderer.render("A WikiWord is in this sentence.", writer);
-    assertEquals("", writer.toString());
+    assertEquals("A <a href='WikiWord'>WikiWord</a> is in this sentence.", writer.toString());
   }
   
 }
