6f92688716cad3e7a355b061acc91ce33f619af5
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 19:48:11 +0000

    Fixed rendering bug with whitespace before a non-initial nested list item."

--- src/net/hillsdon/svnwiki/wiki/renderer/CreoleRenderer.java
+++ src/net/hillsdon/svnwiki/wiki/renderer/CreoleRenderer.java
@@ -55,7 +55,7 @@ public class CreoleRenderer {
   }
   private static class ListNode extends RegexMatchToTag {
     public ListNode(final String match, final String tag) {
-      super("(^|\\n)(\\s*?" + match + "[^*#].*(\\n\\s*|$)([*#]{2}.*(\\n|$))*)+", tag, 0, "(^|\\n)\\s*?[*#]", "$1");
+      super("(^|\\n)(\\s*?" + match + "[^*#].*(\\n|$)(\\s*[*#]{2}.*(\\n|$))*)+", tag, 0, "(^|\\n)\\s*?[*#]", "$1");
     }
   }
 
