89914c3f2a4309913071bdf6a1252bb7c754df94
Matt Hillsdon <matt@hillsdon.net>
2008-03-05 20:32:31 +0000

    Test we delegate remove too.

--- src/net/hillsdon/fij/core/TestIterableUtils.java
+++ src/net/hillsdon/fij/core/TestIterableUtils.java
@@ -5,6 +5,8 @@ import static java.util.Collections.emptyList;
 import static net.hillsdon.fij.core.Functional.list;
 import static net.hillsdon.fij.core.IterableUtils.reversed;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import junit.framework.TestCase;
@@ -12,9 +14,17 @@ import junit.framework.TestCase;
 public class TestIterableUtils extends TestCase {
 
   public void testReversed() {
-    List<Integer> data = asList(1, 2, 3);
+    List<Integer> data = new ArrayList<Integer>(asList(1, 2, 3));
     assertEquals(asList(3, 2, 1), list(reversed(data)));
     assertEquals(emptyList(), list(reversed(emptyList())));
+    
+    Iterator<Integer> iter = reversed(data).iterator();
+    iter.next();
+    iter.next();
+    iter.remove();
+    iter.next();
+    assertFalse(iter.hasNext());
+    assertEquals(asList(1, 3), data);
   }
   
 }
