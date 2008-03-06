ec4256dfeee6eee1d4125d085cd5a9cfb6f56bc0
Matt Hillsdon <matt@hillsdon.net>
2008-03-06 11:00:06 +0000

    More tests.

--- src/net/hillsdon/fij/core/TestFunctional.java
+++ src/net/hillsdon/fij/core/TestFunctional.java
@@ -20,8 +20,14 @@ import static net.hillsdon.fij.core.Functional.filter;
 import static net.hillsdon.fij.core.Functional.iter;
 import static net.hillsdon.fij.core.Functional.list;
 import static net.hillsdon.fij.core.Functional.map;
+import static net.hillsdon.fij.core.Functional.set;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
@@ -41,7 +47,7 @@ public class TestFunctional extends TestCase {
 
   public void testMap() {
     assertEquals(asList(2, 3, 4), list(map(asList(1, 2, 3), new Transform<Integer, Integer>() {
-      public Integer transform(Integer in) {
+      public Integer transform(final Integer in) {
         return in + 1;
       }
     })));
@@ -49,11 +55,62 @@ public class TestFunctional extends TestCase {
 
   public void testFilter() {
     assertEquals(asList(1, 3), list(filter(asList(1, 2, 3), new InversePredicate<Integer>(new Predicate<Integer>() {
-      public Boolean transform(Integer in) {
+      public Boolean transform(final Integer in) {
         return in % 2 == 0;
       }
     }))));
   }
+  
+  public void testCantRemoveFromFilterIterator() {
+    Iterator<Object> iterator = filter(set(""), new Predicate<Object>() {
+      public Boolean transform(final Object in) {
+        return true;
+      }
+    });
+    iterator.next();
+    try {
+      iterator.remove();
+      fail();
+    }
+    catch (UnsupportedOperationException ex) {
+    }
+  }
 
+  public void testCantRemoveFromMapIterator() {
+    Iterator<String> iterator = map(set(""), new Transform<String, String>() {
+      public String transform(final String in) {
+        return "delta " + in;
+      }
+    });
+    iterator.next();
+    try {
+      iterator.remove();
+      fail();
+    }
+    catch (UnsupportedOperationException ex) {
+    }
+  }
+
+  public void testSet() {
+    Set<String> set = set("one", "one", "two");
+    assertTrue(set.contains("one"));
+    assertTrue(set.contains("two"));
+    assertEquals(2, set.size());
+  }
+  
+  public void testSetOfLinkedHashSetIsSame() {
+    LinkedHashSet<String> set = new LinkedHashSet<String>();
+    set(set);
+    assertSame(set, set(set));
+  }
+  
+  public void testSetOfOtherSetIsLinkedHashSet() {
+    assertTrue(set(new HashSet<String>()) instanceof LinkedHashSet);
+  }
+  
+  public void testListOfListIsSame() {
+    ArrayList<String> list = new ArrayList<String>();
+    assertSame(list, list(list));
+  }
   
 }
