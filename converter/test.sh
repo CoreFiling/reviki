e50d3e818b847b4778a17d42b51d5b510b8bfd97
Matt Hillsdon <matt@hillsdon.net>
2008-03-06 11:02:36 +0000

    Output elsewhere.

--- converter/test.sh
+++ converter/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INPUT_DIR="/home/mth/tmp/wiki-data"
-OUTPUT_DIR="/home/mth/tmp/svnwiki-data"
+OUTPUT_DIR="/home/mth/tmp/vq-exports"
 
 rm -rf ~/tmp/svnwiki-data
 ./vqwiki2svnwiki.py "$INPUT_DIR" "$OUTPUT_DIR"
