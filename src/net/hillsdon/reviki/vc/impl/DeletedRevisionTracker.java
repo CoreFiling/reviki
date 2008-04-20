c703c466697ceaf64cb9b2b83624a621cea14007
Matt Hillsdon <matt@hillsdon.net>
2008-04-20 11:24:49 +0000

    Fix typos in comment.

--- src/net/hillsdon/reviki/vc/impl/DeletedRevisionTracker.java
+++ src/net/hillsdon/reviki/vc/impl/DeletedRevisionTracker.java
@@ -21,13 +21,14 @@ import net.hillsdon.reviki.vc.PageStoreAuthenticationException;
 import net.hillsdon.reviki.vc.PageStoreException;
 
 /**
- * SVN provides no way to do svn log or similar on a delete URL
- * without knowing the last URL at which it existed.  They
- * recommend using svn log -v on the parent directory to find
+ * SVN provides no way to do svn log or similar on a deleted URL
+ * without knowing the last URL at which it existed.
+ * 
+ * They recommend using svn log -v on the parent directory to find
  * that revision.  On a wiki with many revisions this is intolerably
  * slow as all the action is in a single directory.
  * 
- * We therefore keep up to date by reading the svn log an remember
+ * We therefore keep up to date by reading the svn log and remember
  * revisions in which files were deleted.
  *
  * @author mth
