75cac632929eb4a8a309b07d05fbb65b684a8202
Matt Hillsdon <matt@hillsdon.net>
2008-05-13 12:01:09 +0000

    Adapt for my convenience.

--- converter/test.sh
+++ converter/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-INPUT_DIR="/home/local/mth/wiki-data"
-OUTPUT_DIR="/home/local/mth/vq-exports"
+INPUT_DIR="/home/mth/tmp/wiki-data"
+OUTPUT_DIR="/home/mth/tmp/vq-exports"
 
 rm -rf "$OUTPUT_DIR"
 ./vqwiki2reviki.py "$INPUT_DIR" "$OUTPUT_DIR"
