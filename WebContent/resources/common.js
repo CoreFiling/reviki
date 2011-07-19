5215e00fc50dff10a0db3822591c22c0ed2cbba9
js <js@corefiling.com>
2011-07-19 14:45:06 +0000

    Merged r1122,r1131 from lzp branch (jquery upgrade, hotkeys fixes, warning on navigate-away)

--- WebContent/resources/common.js
+++ WebContent/resources/common.js
@@ -32,33 +32,52 @@ reviki.configureAutoSuggest = function() {
   }
 }
 
-function shortcutForButton(id, shortcut, confirmMsg) {
+function shortcutForButton(id, shortcutKey, confirmMsg) {
   var button = $("#" + id);
   if (button.length == 1) {
-    jQuery(document).bind('keydown', shortcut, function (evt) {
-      if (confirmMsg != undefined) {
-        var answer = window.confirm(confirmMsg);
-        if (answer) {
-          button.click();
-          return false;
-        }
-        else {
-          return true;
-        }
-      }
-      else {
-        button.click();
-        return false;
-      }
-      
+    $(document).bind('keydown', shortcutKey, function(evt) {
+      return editPageKeyHandler(button, confirmMsg);
     });
+    // disableInInput is not supported in this upgraded version of jquery.hotkeys library, 
+    // but we can't use the previous one, as it doesn't work with the currently used version of jQuery
+    // hence we have to bind the shortcuts to input text fields and textareas manually
+    if(shortcutKey=="esc") {
+      // the esc key has different function for search bar, don't assign it there 
+      inputs = $("input[type=text][name!=query]");
+    }
+    else {
+      inputs = $("input[type=text]");
+    }
+    inputs.bind('keydown', shortcutKey, function(evt) {
+      return editPageKeyHandler(button, confirmMsg);
+    });
+    $("textarea").bind('keydown', shortcutKey, function(evt) {
+      return editPageKeyHandler(button, confirmMsg);
+    });
+  }
+}
+
+function editPageKeyHandler(button, confirmMsg) {
+  if (confirmMsg != undefined) {
+    var answer = window.confirm(confirmMsg);
+    if (answer) {
+      button.click();
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+  else {
+    button.click();
+    return false;
   }
 }
 
 reviki.setupShortcutKeys = function() {
   var editButtonForm = $("#editTop");
   if (editButtonForm.length == 1) {
-    jQuery(document).bind('keydown', {combi:'e', disableInInput: true}, function (evt) {
+    $(document).bind('keydown', 'e', function (evt) {
         editButtonForm.submit();
         return false;
       });
@@ -69,16 +88,63 @@ reviki.setupShortcutKeys = function() {
 
   var searchBar = $("#query");
   if (searchBar.length == 1) {
-    jQuery(document).bind('keydown', {combi:'s', disableInInput: true}, function (evt) {
+    $(document).bind('keydown', 's', function (evt) {
       searchBar.focus();
       return false;
     });
     searchBar.bind('keydown', 'esc', function (evt) {
-    	searchBar.blur();
+      searchBar.blur();
       return false;
     });
   }
   
 }
+
+reviki.confirmLeave = false;
+
+reviki.confirmLeaveFunc = function() {
+  reviki.lockToken = $("input[name=lockToken]").val();
+  if (reviki.confirmLeave) {
+    return "Changes will be lost. The page will be unlocked.";
+  }
+}
+
+reviki.leaving = function() {
+  $.ajax({
+    type: "POST",
+    url: $("#editForm").action,
+    data: "unlock=&lockToken=" + reviki.lockToken,
+    success: function(msg){
+      alert(msg);
+    }
+  });
+
+}
+
+reviki.addConfirm = function() {
+  reviki.confirmLeave = true;  
+}
+
+reviki.removeConfirm = function() {
+  reviki.confirmLeave = false;
+}
+
+reviki.setupLeaveConfirm = function() {
+  if ($("#editForm").length == 1) {
+    if ($("#preview-area").length == 1) {
+      reviki.addConfirm();
+    }
+    else {
+      $("#content").bind("textchange", reviki.addConfirm);
+    }
+    $("#editForm").bind("submit", reviki.removeConfirm);
+    $("#cancel").bind("click", reviki.removeConfirm);
+    $("#save").bind("click", reviki.removeConfirm);
+    window.onunload = reviki.leaving;
+    window.onbeforeunload = reviki.confirmLeaveFunc;
+  }
+}
+
 $(document).ready(reviki.configureAutoSuggest);
 $(document).ready(reviki.setupShortcutKeys);
+$(document).ready(reviki.setupLeaveConfirm);
