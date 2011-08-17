3cb0956f3ff11a4e6d0f7f416fb41f7fb9ca9b2a
js <js@corefiling.com>
2011-08-17 23:08:57 +0000

    Tidy up and remove duplicate code from tests introduced in r1204 https://bugs.corefiling.com/show_bug.cgi?id=10541 https://bugs.corefiling.com/show_bug.cgi?id=10725

--- src/net/hillsdon/reviki/wiki/renderer/creole/TestAbstractRegexNode.java
+++ src/net/hillsdon/reviki/wiki/renderer/creole/TestAbstractRegexNode.java
@@ -3,8 +3,6 @@ package net.hillsdon.reviki.wiki.renderer.creole;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import net.hillsdon.reviki.vc.PageInfo;
 import net.hillsdon.reviki.vc.impl.PageInfoImpl;
 import net.hillsdon.reviki.web.urls.URLOutputFilter;
@@ -15,65 +13,50 @@ import junit.framework.TestCase;
 
 public class TestAbstractRegexNode extends TestCase {
 
-  public void testRendering() {
-    RenderNode node1 = new AbstractRegexNode("testNode") {
-      public ResultNode handle(PageInfo page, Matcher matcher, RenderNode parent, URLOutputFilter urlOutputFilter) throws URISyntaxException, UnknownWikiException {
-        return new HtmlEscapeResultNode("processed1");
-      }
-      @Override
-      public Matcher find(String text) {
-        Pattern pattern = Pattern.compile("\\p{Alnum}+:\\p{Alnum}+");
-        Matcher matcher = pattern.matcher(text);
-        return matcher.find() ? matcher : null;
-      }
-    };
+  private static final String WIKI_CONTENT = "sometext some:wiki some::moretext";
 
-    RenderNode node2 = new AbstractRegexNode("testNode2") {
-      public ResultNode handle(PageInfo page, Matcher matcher, RenderNode parent, URLOutputFilter urlOutputFilter) throws URISyntaxException, UnknownWikiException {
-        return new HtmlEscapeResultNode("processed2");
-      }
-      @Override
-      public Matcher find(String text) {
-        Pattern pattern = Pattern.compile("\\p{Alnum}+::\\p{Alnum}+");
-        Matcher matcher = pattern.matcher(text);
-        return matcher.find() ? matcher : null;
-      }
-    };
+  private static final String RE_LINK_STYLE_1 = "\\p{Alnum}+:\\p{Alnum}+";
+
+  private static final String RE_LINK_STYLE_2 = "\\p{Alnum}+::\\p{Alnum}+";
+
+  public void testRendering() {
+    RenderNode node1 = new AbstractRegexNodeStub(RE_LINK_STYLE_1, "processed1", false);
+    RenderNode node2 = new AbstractRegexNodeStub(RE_LINK_STYLE_2, "processed2", false);
     node1.addChildren(node1);
     node1.addChildren(node2);
-    List<ResultNode> result = node1.render(new PageInfoImpl("SomePage"), "sometext some:wiki some::moretext", null, null);
+    List<ResultNode> result = node1.render(new PageInfoImpl("SomePage"), WIKI_CONTENT, null, null);
     CompositeResultNode composite = new CompositeResultNode(result);
     assertEquals("sometext processed1 processed2", composite.toXHTML());
   }
 
   public void testRenderingWithException() {
-    // test that throwing an exception doesn't affect rendering of the rest of the text
-    RenderNode node1 = new AbstractRegexNode("testNode") {
-      public ResultNode handle(PageInfo page, Matcher matcher, RenderNode parent, URLOutputFilter urlOutputFilter) throws URISyntaxException, UnknownWikiException {
-        throw new UnknownWikiException();
-      }
-      @Override
-      public Matcher find(String text) {
-        Pattern pattern = Pattern.compile("\\p{Alnum}+:\\p{Alnum}+");
-        Matcher matcher = pattern.matcher(text);
-        return matcher.find() ? matcher : null;
-      }
-    };
-    RenderNode node2 = new AbstractRegexNode("testNode2") {
-      public ResultNode handle(PageInfo page, Matcher matcher, RenderNode parent, URLOutputFilter urlOutputFilter) throws URISyntaxException, UnknownWikiException {
-        return new HtmlEscapeResultNode("processed");
-      }
-      @Override
-      public Matcher find(String text) {
-        Pattern pattern = Pattern.compile("\\p{Alnum}+::\\p{Alnum}+");
-        Matcher matcher = pattern.matcher(text);
-        return matcher.find() ? matcher : null;
-      }
-    };
+    // test that throwing an exception in node.handle() doesn't affect rendering of the rest of
+    // the text
+    RenderNode node1 = new AbstractRegexNodeStub(RE_LINK_STYLE_1, "processed1", true);
+    RenderNode node2 = new AbstractRegexNodeStub(RE_LINK_STYLE_2, "processed2", false);
     node1.addChildren(node1);
     node1.addChildren(node2);
-    List<ResultNode> result = node1.render(new PageInfoImpl("SomePage"), "sometext some:wiki some::moretext", null, null);
+    List<ResultNode> result = node1.render(new PageInfoImpl("SomePage"), WIKI_CONTENT, null, null);
     CompositeResultNode composite = new CompositeResultNode(result);
-    assertEquals("sometext some:wiki processed", composite.toXHTML());
+    assertEquals("sometext some:wiki processed2", composite.toXHTML());
+  }
+
+  private static class AbstractRegexNodeStub extends AbstractRegexNode {
+    private final String _replacementText;
+
+    private final boolean _shouldThrow;
+
+    private AbstractRegexNodeStub(String matchRe, String replacementText, boolean shouldThrow) {
+      super(matchRe);
+      _replacementText = replacementText;
+      _shouldThrow = shouldThrow;
+    }
+
+    public ResultNode handle(PageInfo page, Matcher matcher, RenderNode parent, URLOutputFilter urlOutputFilter) throws URISyntaxException, UnknownWikiException {
+      if (_shouldThrow) {
+        throw new UnknownWikiException();
+      }
+      return new HtmlEscapeResultNode(_replacementText);
+    }
   }
 }
