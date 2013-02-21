859e3eba230beaf0059043b51d3be2e1661fc1c5
pwc <pwc@corefiling.com>
2013-02-21 18:18:47 +0000

    Add a timing test for rendering a page containing interwiki links.
    https://bugs.corefiling.com/show_bug.cgi?id=14571

--- webtests/net/hillsdon/reviki/webtests/TestInterWikiLinks.java
+++ webtests/net/hillsdon/reviki/webtests/TestInterWikiLinks.java
@@ -15,6 +15,8 @@
  */
 package net.hillsdon.reviki.webtests;
 
+import java.io.IOException;
+
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -24,11 +26,45 @@ public class TestInterWikiLinks extends WebTestSupport {
   /**
    * Add an interwiki link mapping, create a link, make sure it points where we expect.
    */
-  public void test() throws Exception {
+  public void testLink() throws Exception {
     editWikiPage("ConfigInterWikiLinks", "foo http://www.example.com/Wiki?%s", "", "Link mapping.", null);
-    HtmlPage page = editWikiPage(uniqueWikiPageName("InterWikiLinkTest"), "foo:1234", "", "Add inter-wiki link.", true);
+    HtmlPage page = editWikiPage(uniqueWikiPageName("InterWikiLinkTest"), "foo:1234", "", "Add inter-wiki links.", true);
     HtmlAnchor link = page.getAnchorByHref("http://www.example.com/Wiki?1234");
     assertEquals("foo:1234", link.asText());
   }
+  
+  /**
+   * Add many interiki links to a page and test the render time.
+   */
+  public void testRenderTiming() throws Exception {
+    editWikiPage("ConfigInterWikiLinksTiming", "foo http://www.example.com/Wiki?%s", "", "Link mapping.", null);
+    String content = "";
+    for (int i=0; i<100; i++) {
+      content += "foo:" + i + " ";
+    }
+    String pageName = uniqueWikiPageName("ConfigInterWikiLinkTest");
+    editWikiPage(pageName, content, "", "Add inter-wiki links.", true);
 
+    assertTrue(wikiPageFetchTime(pageName, 3) < 1000);
+  }
+
+  /**
+   * Fetch a wiki page several times and return the shortest elapsed time.
+   */
+  public long wikiPageFetchTime(String pageName, int iterations) throws IOException {
+    long minElapsedTime = -1;
+
+    for (int i=0; i<iterations; i++) {
+      long startTime = System.currentTimeMillis();
+      getWikiPage(pageName);
+      long endTime = System.currentTimeMillis();
+
+      long elapsedTime = endTime - startTime;
+      if (minElapsedTime == -1 || elapsedTime < minElapsedTime) {
+        minElapsedTime = elapsedTime;
+      }
+    }
+    System.out.println(minElapsedTime);
+    return minElapsedTime;
+  }
 }
