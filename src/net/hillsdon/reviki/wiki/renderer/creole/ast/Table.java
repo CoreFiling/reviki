d1f16232a6425cc4d91ff254510176c1e4dbca23
Michael Walker <mike@barrucadu.co.uk>
2014-07-29 12:49:20 +0100

    Implement a directive for table cell vertical alignment.
    Adds a directive `table-alignment` which assumes (at least, but more
    ignored) one argument, which is a valid value for the CSS vertical-align
    property. To be more forwards-compatible, it should probably be "top",
    "middle", or "bottom".

--- src/net/hillsdon/reviki/wiki/renderer/creole/ast/Table.java
+++ src/net/hillsdon/reviki/wiki/renderer/creole/ast/Table.java
@@ -3,6 +3,8 @@ package net.hillsdon.reviki.wiki.renderer.creole.ast;
 import java.util.List;
 
 public class Table extends TaggedNode {
+  public static final String TABLE_ALIGNMENT_DIRECTIVE = "table-alignment";
+
   public Table(final List<ASTNode> rows) {
     super("table", rows);
   }
--- src/net/hillsdon/reviki/wiki/renderer/creole/ast/TableCell.java
+++ src/net/hillsdon/reviki/wiki/renderer/creole/ast/TableCell.java
@@ -1,7 +1,29 @@
 package net.hillsdon.reviki.wiki.renderer.creole.ast;
 
+import java.util.List;
+import java.util.Map;
+
 public class TableCell extends TaggedNode {
   public TableCell(final ASTNode body) {
     super("td", body);
+  }
+
+  @Override
+  public String toXHTML(Map<String, List<String>> enabledDirectives) {
+    if (!enabledDirectives.containsKey(Table.TABLE_ALIGNMENT_DIRECTIVE)) {
+      return super.toXHTML(enabledDirectives);
+    }
+
+    try {
+      String out = "<td " + CSS_CLASS_ATTR;
+      out += " style='vertical-align:" + enabledDirectives.get(Table.TABLE_ALIGNMENT_DIRECTIVE).get(0) + "'>";
+      out += innerXHTML(enabledDirectives);
+      out += "</td>";
+      return out;
+    }
+    catch (Exception e) {
+      System.err.println("Error when handling directive " + Table.TABLE_ALIGNMENT_DIRECTIVE);
+      return super.toXHTML(enabledDirectives);
+    }
   }
 }
